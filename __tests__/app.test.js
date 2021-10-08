3ad84578e18b341459080eb9f1feb19b2a849e98
TANNER MECK <steve.meck246@gmail.com>
2021-10-08 16:27:08 -0700

    user created

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,6 +8,15 @@ describe('authentication practice routes', () => {
     return setup(pool);
   });
 
+  it('should post a user using /signup route', async () => {
+    return request(app)
+      .post('/api/auth/signup')
+      .send({ email: 'tanner@alchemy.com', password: 'password' })
+      .then((response) => {
+        expect(response.body).toEqual({ id: '1' });
+      });
+  });
+
   afterAll(() => {
     pool.end();
   });
